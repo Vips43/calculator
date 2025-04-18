672cd6b1921ba94c9fdbfe9b6350d012d7d3cbae
Vips43 <vipulgehlot43@gmail.com>
2025-04-18 16:32:38 +0530

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,19 @@
+
+-----A Calculator with simple User freindly design-----
+
+
+
+
+
+
+you can do simple calculations with click oer numbers and operators,
+
+you cannot be able to start a number with '0' and also with operators like '/' , '%', '*', '.'
+
+click on numbers as you want to get an output and then hit '=' button to get an answer.
+ 
+
+
+
+
+Thank you
---
+++ scripts/script.js
@@ -0,0 +1,59 @@
+let display = document.getElementById('outputscreen');
+let prev = document.getElementById('out');
+
+function myFunction(input){
+    if(display.value.startsWith('0',0)){  //prevents user from start number with '0'
+        display.value = "";
+    }
+    else if(display.value.startsWith('%',0)){  //prevents user from start number with '%'
+        display.value = "";
+    }
+    else if(display.value.startsWith('/',0)){  //prevents user from start number with '/'
+        display.value = "";
+    }
+    else if(display.value.startsWith('*',0)){  //prevents user from start number with '*'
+        display.value = "";
+    
+    }
+    else{  // take user input
+        display.value += input;
+        // prev.value = display.value;
+
+    }
+}
+function prevClear(){
+    prev.value = "";
+}
+
+// clear display function it clear a display once
+
+function clearDisplay(){
+    display.value = "";
+}
+
+// removes a number from last like back spcae
+
+
+function backSpace(){   
+    
+    display.value = display.value.slice(0,-1);
+}
+
+// calculate function when user press = button it gives ouput
+function calc(){
+    try{
+        prev.value = display.value;
+        if(display.value == ''){
+
+        }
+        var result = eval(display.value);
+        display.value = '=' + result;
+        
+    }
+    //  error catching when user enter inapropriate value it says eror   
+    catch(err){
+        display.value ="error";
+    }
+}
+
+   
